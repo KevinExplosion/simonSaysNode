6f41c99e403b8f22ba32b939a29c901601dafd8b
Kevin Mattison and Michael Schennum <mikeschenphoto@gmail.com>
2016-03-14 16:48:16 -0700

    adds color sequence

--- js/simon.js
+++ js/simon.js
@@ -19,15 +19,23 @@ $(document).ready(function(){
     var removeColor = newSimon.newRound();
     console.log(removeColor);
     for(i=0; i < removeColor.length; i++) {
-      if(removeColor[i] === 1) {
-        $('#1').removeClass('btn-danger');
-      } else if(removeColor[i] === 2) {
-        $('#2').removeClass('btn-primary');
-      } else if(removeColor[i] === 3) {
-        $('#3').removeClass('btn-warning');
-      } else if(removeColor[i] === 4) {
-        $('#4').removeClass('btn-success');
-      }
+        if(removeColor[i] === 1) {
+            $('#1').removeClass('btn-danger');
+            setTimeout(function() {
+              $('#1').addClass('btn-danger');}, 2000);
+        } else if(removeColor[i] === 2) {
+          $('#2').removeClass('btn-primary');
+          setTimeout(function() {
+            $('#2').addClass('btn-primary');}, 2000);
+        } else if(removeColor[i] === 3) {
+          $('#3').removeClass('btn-warning');
+          setTimeout(function() {
+            $('#3').addClass('btn-warning');}, 2000);
+        } else if(removeColor[i] === 4) {
+          $('#4').removeClass('btn-success');
+          setTimeout(function() {
+            $('#4').addClass('btn-success');}, 2000);
+        }
     }
   });
 });
