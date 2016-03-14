b731164d513056498dcd351b3d79e33b469e6342
Kevin Mattison and Michael Schennum <mikeschenphoto@gmail.com>
2016-03-14 16:29:17 -0700

    removes color on tiles

--- js/simon.js
+++ js/simon.js
@@ -1,20 +1,39 @@
 Simon = function() {
   this.sequence= [];
+  this.output = [];
 };
 
 Simon.prototype.newRound = function() {
-  this.sequence.push(this.getRandom());
-  return this.sequence;
+  this.sequence.push(this.randomNumber());
+  return(this.sequence);
 };
 
-Simon.prototype.getRandom = function() {
-    return Math.floor((Math.random()*4)+1);
-};
+Simon.prototype.randomNumber = function() {
+  return Math.floor((Math.random()*4)+1);
+}
 
 $(document).ready(function(){
+  var newSimon = new Simon();
   $('#newgame').submit(function(event){
     event.preventDefault();
-    var newSimon = new Simon();
-    $('#final').append(newSimon.newRound());
+    var removeColor = newSimon.newRound();
+    console.log(removeColor);
+    for(i=0; i < removeColor.length; i++) {
+      if(removeColor[i] === 1) {
+        $('#1').removeClass('btn-danger');
+      } else if(removeColor[i] === 2) {
+        $('#2').removeClass('btn-primary');
+      } else if(removeColor[i] === 3) {
+        $('#3').removeClass('btn-warning');
+      } else if(removeColor[i] === 4) {
+        $('#4').removeClass('btn-success');
+      }
+    }
   });
 });
+
+// for "this.sequence" (i >= 0, i < sequence.length, i++){
+// if(i = 1) {
+// click "#1"
+// }
+// }
